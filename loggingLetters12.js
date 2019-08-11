23e3e4aa5abf8aabe7763a9cac439a877dcd9818
teontre <52181284+teontre@users.noreply.github.com>
2019-08-10 19:29:52 -0500

    Add files via upload

---
+++ loggingLetters12.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript" src="loggingLetters.js"></script>
+
+<script> 
+function validateform()                                    
+{ 
+    var name = document.forms["RegForm"]["Name"];               
+    var email = document.forms["RegForm"]["EMail"];    
+    var phone = document.forms["RegForm"]["Telephone"];  
+    var what =  document.forms["RegForm"]["Subject"];  
+    var password = document.forms["RegForm"]["Password"];  
+    var address = document.forms["RegForm"]["Address"];  
+    }
+</head>
+
+
+<body>
+        
+  <h1>Simple Example Page</h1>
+    <script type="text/javascript">
+      document.write("Hi, JavaScript is enabled!");
+    </script>
+    <noscript>
+      <div style="border: 1px solid purple; padding: 10px">
+        <span style="color:red">JavaScript is not enabled!</span>
+      </div>
+    </noscript>       
+        
+        
+ <form action="/action_page.php" method="post">
+  <input type="text" name="fname" required>
+  <input type="submit" value="Submit">
+</form>
+
+
+        
+</body>
+</html>
+
+
+
